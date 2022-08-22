488061dd4ef8fdf3ac7329e12f4269000f1cc99e
root <chaubnphan@gmail.com>
2022-08-21 22:32:37 -0700

    Create a function that sorts an array of int using the Bubble sort algorithm

--- sort.h
+++ sort.h
@@ -1,5 +1,5 @@
-#ifndef SORT_H
-#define SORT_H
+#ifndef _SORT_H_
+#define _SORT_H_
 
 #include <stdio.h>
 #include <stdlib.h>
@@ -13,9 +13,9 @@
  */
 typedef struct listint_s
 {
-    const int n;
-    struct listint_s *prev;
-    struct listint_s *next;
+	const int n;
+	struct listint_s *prev;
+	struct listint_s *next;
 } listint_t;
 
 void print_array(const int *array, size_t size);
@@ -25,4 +25,4 @@ void insertion_sort_list(listint_t **list);
 void selection_sort(int *array, size_t size);
 void quick_sort(int *array, size_t size);
 
-#endif
+#endif /* _SORT_H_ */
